99963f414919c3b26dca85cceeda05b63ef18d02
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-04-20 22:14:12 +0300

    Category List and Category Create pages created

--- resources/views/management/createCategory.blade.php
+++ resources/views/management/createCategory.blade.php
@@ -11,7 +11,17 @@
                     <a class="list-group-item list-group-item-action" href="#"><i class="fas fa-users"></i> User</a>
                 </div>
             </div>
-            <div class="col-md-8">Content</div>
+            <div class="col-md-8">
+                Create a Category
+                <hr>
+                <form action="">
+                    <div class="form-group">
+                        <label for="categoryName">Category Name</label>
+                        <input type="text" name="name" class="form-control" placeholder="Category...">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
