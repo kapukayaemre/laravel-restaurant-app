7bc51dc0217303f1a5b1aaccc27ba268863da279
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-04-21 03:13:47 +0300

    Menus listed on index page and relations created between Menu and Category

--- app/Http/Controllers/Management/MenuController.php
+++ app/Http/Controllers/Management/MenuController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Management;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Category;
+use App\Menu;
 
 
 class MenuController extends Controller
@@ -16,7 +17,8 @@ class MenuController extends Controller
      */
     public function index()
     {
-        return view('management.menu');
+        $menus = Menu::all();
+        return view('management.menu')->with('menus',$menus);
     }
 
     /**
@@ -38,7 +40,37 @@ class MenuController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name'          => 'required|unique:menus|max:255',
+            'price'         => 'required|numeric',
+            'category_id'   => 'required|numeric'
+        ]);
+
+        // Kullanıcı Resim Yüklemezse default olarak bu isim veritabanına kayıt edilecek ve ilgili resim gösterilecek.
+        $imageName = "noimage.png";
+
+        // Eğer Kullanıcı Resim Yüklerse Çalışacak Kodlar
+        if ($request->image)
+        {
+            $request->validate([
+               'image'  => 'nullable|file|image|mimes:jpeg,png,jpg|max:5000'
+            ]);
+            $imageName = date('mdYHis').uniqid().'.'.$request->image->extension();
+            $request->image->move(public_path('menu_images'), $imageName);
+        }
+        // Menü Tablosuna Verileri Kayıt Etme İşlemleri
+        $menu = new Menu(); // Bu Şekilde menu değişkeninde veritabanı columnlara eriştik.
+        $menu->name = $request->name;
+        $menu->price = $request->price;
+        $menu->image = $imageName;
+        $menu->description = $request->description;
+        $menu->category_id = $request->category_id;
+        $menu->save();
+
+        // Bildirim Gönderiyoruz
+        $request->session()->flash('status', $request->name. " is saved successfully");
+        return redirect('/management/menu');
+
     }
 
     /**
--- resources/views/management/createMenu.blade.php
+++ resources/views/management/createMenu.blade.php
@@ -3,15 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="list-group">
-                    <a class="list-group-item list-group-item-action" href="/management/category"><i
-                            class="fas fa-align-justify"></i> Category</a>
-                    <a class="list-group-item list-group-item-action" href="#"><i class="fas fa-hamburger"></i> Menu</a>
-                    <a class="list-group-item list-group-item-action" href="#"><i class="fas fa-chair"></i> Table</a>
-                    <a class="list-group-item list-group-item-action" href="#"><i class="fas fa-users"></i> User</a>
-                </div>
-            </div>
+            @include('management.inc.sidebar')
             <div class="col-md-8">
                 Create a Menu
                 <hr>
@@ -27,7 +19,7 @@
                         </ul>
                     </div>
                 @endif
-                <form action="/management/menu" method="POST">
+                <form action="/management/menu" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="form-group">
                         <label for="menuName">Menu Name</label>
