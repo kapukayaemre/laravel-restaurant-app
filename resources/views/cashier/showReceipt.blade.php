0cfce6837345a973489e5bf04f51a065716abb29
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-04-24 02:16:39 +0300

    Receipt page css integrated and Report Page created

--- resources/views/cashier/showReceipt.blade.php
+++ resources/views/cashier/showReceipt.blade.php
@@ -6,6 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Restaurant App - Receipt - SaleID: {{ $sale->id }} </title>
+    <link rel="stylesheet" href="{{ asset('/css/receipt.css') }}" media="all">
+    <link rel="stylesheet" href="{{ asset('/css/no-print.css') }}" media="print">
 </head>
 <body>
 <div id="wrapper">
---
+++ resources/views/report/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                @if($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li>
+                                    <button type="button" class="close" data-dismiss="alert">X</button>
+                                    {{ $error }}
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/home">Main Functions</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Report</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+@endsection
