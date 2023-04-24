c316e05b19ea9b022e3b4e3d17bbdfab655d3851
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-04-24 19:09:23 +0300

    increase and decrease button integrated

--- app/Http/Controllers/Cashier/CashierController.php
+++ app/Http/Controllers/Cashier/CashierController.php
@@ -23,7 +23,7 @@ class CashierController extends Controller
     public function getTables()
     {
         $tables = Table::all();
-        $html = '';
+        $html   = '';
         foreach ($tables as $table) {
             $html .= '<div class="col-md-2 mb-4">';
             $html .=
@@ -45,7 +45,7 @@ class CashierController extends Controller
     public function getMenuByCategory($category_id)
     {
         $menus = Menu::where('category_id', $category_id)->get();
-        $html = '';
+        $html  = '';
         foreach ($menus as $menu) {
             $html .= '
             <div class="col-md-3 text-center">
@@ -66,22 +66,22 @@ class CashierController extends Controller
 
     public function orderFood(Request $request)
     {
-        $menu = Menu::find($request->menu_id);
-        $table_id = $request->table_id;
+        $menu       = Menu::find($request->menu_id);
+        $table_id   = $request->table_id;
         $table_name = $request->table_name;
-        $sale = Sale::where('table_id', $table_id)->where('sale_status', 'unpaid')->first();
+        $sale       = Sale::where('table_id', $table_id)->where('sale_status', 'unpaid')->first();
         // if there is no sale for the selected table, create a new sale record
         if (!$sale) {
-            $user = Auth::user();
-            $sale = new Sale();
-            $sale->table_id = $table_id;
+            $user             = Auth::user();
+            $sale             = new Sale();
+            $sale->table_id   = $table_id;
             $sale->table_name = $table_name;
-            $sale->user_id = $user->id;
-            $sale->user_name = $user->name;
+            $sale->user_id    = $user->id;
+            $sale->user_name  = $user->name;
             $sale->save();
             $sale_id = $sale->id;
             // update table status
-            $table = Table::find($table_id);
+            $table         = Table::find($table_id);
             $table->status = "unavailable";
             $table->save();
         } else { // if there is a sale on the selected table
@@ -89,12 +89,12 @@ class CashierController extends Controller
         }
 
         // add ordered menu to the sale_details table
-        $saleDetail = new SaleDetail();
-        $saleDetail->sale_id = $sale_id;
-        $saleDetail->menu_id = $menu->id;
-        $saleDetail->menu_name = $menu->name;
+        $saleDetail             = new SaleDetail();
+        $saleDetail->sale_id    = $sale_id;
+        $saleDetail->menu_id    = $menu->id;
+        $saleDetail->menu_name  = $menu->name;
         $saleDetail->menu_price = $menu->price;
-        $saleDetail->quantity = $request->quantity;
+        $saleDetail->quantity   = $request->quantity;
         $saleDetail->save();
         //update total price in the sales table
         $sale->total_price = $sale->total_price + ($request->quantity * $menu->price);
@@ -110,7 +110,7 @@ class CashierController extends Controller
         $html = '';
         if ($sale) {
             $sale_id = $sale->id;
-            $html .= $this->getSaleDetails($sale_id);
+            $html    .= $this->getSaleDetails($sale_id);
         } else {
             $html .= "Not Found Any Sale Details for the Selected Table";
         }
@@ -121,9 +121,9 @@ class CashierController extends Controller
     private function getSaleDetails($sale_id)
     {
         // list all saledetail
-        $html = '<p>Sale ID: ' . $sale_id . '</p>';
-        $saleDetails = SaleDetail::where('sale_id', $sale_id)->get();
-        $html .= '<div class="table-responsive-md" style="overflow-y:scroll; height: 400px; border: 1px solid #343A40">
+        $html           = '<p>Sale ID: ' . $sale_id . '</p>';
+        $saleDetails    = SaleDetail::where('sale_id', $sale_id)->get();
+        $html           .= '<div class="table-responsive-md" style="overflow-y:scroll; height: 400px; border: 1px solid #343A40">
         <table class="table table-stripped table-dark">
         <thead>
             <tr>
@@ -139,16 +139,21 @@ class CashierController extends Controller
         $showBtnPayment = true;
         foreach ($saleDetails as $saleDetail) {
 
+            $decreaseButton = '<button class="btn btn-decrease-quantity text-danger" disabled>-</button>';
+            if ($saleDetail->quantity > 1) {
+                $decreaseButton = '<button data-id="' . $saleDetail->id . '" class="btn btn-decrease-quantity text-danger">-</button>';
+            }
+
             $html .= '
             <tr>
                 <td>' . $saleDetail->menu_id . '</td>
                 <td>' . $saleDetail->menu_name . '</td>
-                <td>' . $saleDetail->quantity . '</td>
+                <td>' . $decreaseButton. ' '. $saleDetail->quantity . '<button data-id="' . $saleDetail->id . '" class="btn btn-increase-quantity text-success">+</button></td>
                 <td>' . $saleDetail->menu_price . '</td>
                 <td>' . ($saleDetail->menu_price * $saleDetail->quantity) . '</td>';
             if ($saleDetail->status == "noConfirm") {
                 $showBtnPayment = false;
-                $html .= '<td><a data-id="' . $saleDetail->id . '" class="btn btn-danger btn-delete-saledetail"><i class="far fa-trash-alt"></a></td>';
+                $html           .= '<td><a data-id="' . $saleDetail->id . '" class="btn btn-danger btn-delete-saledetail"><i class="far fa-trash-alt"></a></td>';
             } else { // status == "confirm"
                 $html .= '<td><i class="fas fa-check-circle"></i></td>';
             }
@@ -170,24 +175,58 @@ class CashierController extends Controller
         return $html;
     }
 
+    public function increaseQuantity(Request $request)
+    {
+        $saleDetail_id        = $request->saleDetail_id;
+        // update quantity
+        $saleDetail           = SaleDetail::where('id', $saleDetail_id)->first();
+        $saleDetail->quantity = $saleDetail->quantity + 1;
+        $saleDetail->save();
+
+        // update total amount
+        $sale = Sale::where('id', $saleDetail->sale_id)->first();
+        $sale->total_price = $sale->total_price + $saleDetail->menu_price;
+        $sale->save();
+
+        $html = $this->getSaleDetails($saleDetail->sale_id);
+        return $html;
+    }
+
+    public function decreaseQuantity(Request $request)
+    {
+        $saleDetail_id        = $request->saleDetail_id;
+        // update quantity
+        $saleDetail           = SaleDetail::where('id', $saleDetail_id)->first();
+        $saleDetail->quantity = $saleDetail->quantity - 1;
+        $saleDetail->save();
+
+        // update total amount
+        $sale = Sale::where('id', $saleDetail->sale_id)->first();
+        $sale->total_price = $sale->total_price - $saleDetail->menu_price;
+        $sale->save();
+
+        $html = $this->getSaleDetails($saleDetail->sale_id);
+        return $html;
+    }
+
     public function confirmOrderStatus(Request $request)
     {
-        $sale_id = $request->sale_id;
+        $sale_id     = $request->sale_id;
         $saleDetails = SaleDetail::where('sale_id', $sale_id)->update(['status' => 'confirm']);
-        $html = $this->getSaleDetails($sale_id);
+        $html        = $this->getSaleDetails($sale_id);
         return $html;
     }
 
     public function deleteSaleDetail(Request $request)
     {
         $saleDetail_id = $request->saleDetail_id;
-        $saleDetail = SaleDetail::find($saleDetail_id);
-        $sale_id = $saleDetail->sale_id;
-        $menu_price = ($saleDetail->menu_price * $saleDetail->quantity);
+        $saleDetail    = SaleDetail::find($saleDetail_id);
+        $sale_id       = $saleDetail->sale_id;
+        $menu_price    = ($saleDetail->menu_price * $saleDetail->quantity);
         $saleDetail->delete();
 
         // update total price
-        $sale = Sale::find($sale_id);
+        $sale              = Sale::find($sale_id);
         $sale->total_price = $sale->total_price - $menu_price;
         $sale->save();
 
@@ -203,29 +242,29 @@ class CashierController extends Controller
 
     public function savePayment(Request $request)
     {
-        $saleID = $request->saleID;
+        $saleID         = $request->saleID;
         $recievedAmount = $request->recievedAmount;
-        $paymentType = $request->paymentType;
+        $paymentType    = $request->paymentType;
 
         // update sale information in the sales table by using sale model
-        $sale = Sale::find($saleID);
+        $sale                 = Sale::find($saleID);
         $sale->total_recieved = $recievedAmount;
-        $sale->change = $recievedAmount - $sale->total_price;
-        $sale->payment_type = $paymentType;
-        $sale->sale_status = "paid";
+        $sale->change         = $recievedAmount - $sale->total_price;
+        $sale->payment_type   = $paymentType;
+        $sale->sale_status    = "paid";
         $sale->save();
 
         // update table to be available
-        $table = Table::find($sale->table_id);
+        $table         = Table::find($sale->table_id);
         $table->status = "available";
         $table->save();
-        return "/cashier/showReceipt/".$saleID;
+        return "/cashier/showReceipt/" . $saleID;
     }
 
     public function showReceipt($saleID)
     {
-        $sale = Sale::find($saleID);
-        $saleDetails = SaleDetail::where('sale_id',$saleID)->get();
+        $sale        = Sale::find($saleID);
+        $saleDetails = SaleDetail::where('sale_id', $saleID)->get();
         return view('cashier.showReceipt')->with('sale', $sale)->with('saleDetails', $saleDetails);
     }
 
--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -157,6 +157,38 @@
                 })
             });
 
+            // increase quantity
+            $("#order-detail").on('click', '.btn-increase-quantity', function () {
+                var SaleDetailID = $(this).data("id");
+                $.ajax({
+                    type: "POST",
+                    data: {
+                        "_token": $('meta[name="csrf-token"]').attr('content'),
+                        "saleDetail_id": SaleDetailID
+                    },
+                    url: "/cashier/increase-quantity",
+                    success: function (data) {
+                        $("#order-detail").html(data);
+                    }
+                })
+            });
+
+            //decrease quantity
+            $("#order-detail").on('click', '.btn-decrease-quantity', function () {
+                var SaleDetailID = $(this).data("id");
+                $.ajax({
+                    type: "POST",
+                    data: {
+                        "_token": $('meta[name="csrf-token"]').attr('content'),
+                        "saleDetail_id": SaleDetailID
+                    },
+                    url: "/cashier/decrease-quantity",
+                    success: function (data) {
+                        $("#order-detail").html(data);
+                    }
+                })
+            });
+
             // when a user click on the payment button
             $("#order-detail").on("click", ".btn-payment", function () {
                 var totalAmount = $(this).attr('data-totalAmount');
